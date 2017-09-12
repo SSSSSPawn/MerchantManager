bec60cd720d432ea578db2bc0acdf0517f7853e8
SSSSSPawn <swmyun@163.com>
2017-09-12 18:06:18 +0800

    ---网络请求---

---
+++ app/src/main/java/com/iotek/merchantmanager/net/ApiService.java
@@ -0,0 +1,9 @@
+package com.iotek.merchantmanager.net;
+
+/**
+ * Created by admin on 2017/9/12.
+ */
+
+public interface ApiService {
+
+}
---
+++ app/src/main/java/com/iotek/merchantmanager/net/HttpExecutor.java
@@ -0,0 +1,13 @@
+package com.iotek.merchantmanager.net;
+
+/**
+ * Created by admin on 2017/9/12.
+ */
+
+public class HttpExecutor {
+
+    public static final String API_URL="http://192.168.0.150:8080/";
+
+    private ApiService mApiService;
+
+}
