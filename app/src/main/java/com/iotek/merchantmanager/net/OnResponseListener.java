3325beaf19e7c62e6a75139d9db6edd78b3755b1
SSSSSPawn <swmyun@163.com>
2017-09-14 23:35:30 +0800

    ----OnResponseListener 回调----

--- app/src/main/java/com/iotek/merchantmanager/net/OnResponseListener.java
+++ app/src/main/java/com/iotek/merchantmanager/net/OnResponseListener.java
@@ -1,5 +1,11 @@
 package com.iotek.merchantmanager.net;
 
+import android.app.Activity;
+import android.content.Context;
+import android.os.Build;
+
+import com.iotek.merchantmanager.view.LoadingDialog;
+
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
@@ -10,6 +16,30 @@ import retrofit2.Response;
 
 public class OnResponseListener<T> implements Callback<T> {
 
+    private Context mContext;
+
+    private LoadingDialog mLoadingDialog;
+
+    private boolean isShowDialog;
+
+
+    public OnResponseListener(Context context) {
+        this(context,true);
+    }
+
+    public OnResponseListener(Context context, boolean isShow) {
+        mContext = context;
+        isShowDialog = isShow;
+        mLoadingDialog = LoadingDialog.show(context);
+        if (isShowDialog && context != null) {
+            mLoadingDialog = new LoadingDialog(context);
+        }
+    }
+
+    public boolean isShowDialog() {
+        return isShowDialog;
+    }
+
     @Override
     public void onResponse(Call<T> call, Response<T> response) {
 
@@ -18,5 +48,24 @@ public class OnResponseListener<T> implements Callback<T> {
     @Override
     public void onFailure(Call<T> call, Throwable t) {
 
+    }
+
+    public void dismissDialog() {
+        if (mLoadingDialog != null && mContext != null && mLoadingDialog.isShowing()){
+            if (mContext instanceof Activity){
+                Activity activity = (Activity) mContext;
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1){
+                    if (activity.isDestroyed()){
+                        mLoadingDialog = null;
+                    }else {
+                        mLoadingDialog.dismiss();
+                    }
+                }else {
+                    mLoadingDialog.dismiss();
+                }
+            }else {
+                mLoadingDialog.dismiss();
+            }
+        }
     }
 }
