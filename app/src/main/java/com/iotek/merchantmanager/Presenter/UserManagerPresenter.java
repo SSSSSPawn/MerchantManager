c61ffbc0960959f9d91cd50f057d222cab1ac9d6
SSSSSPawn <swmyun@163.com>
2017-09-18 16:41:00 +0800

    ---查询接口调试--

--- app/src/main/java/com/iotek/merchantmanager/Presenter/UserManagerPresenter.java
+++ app/src/main/java/com/iotek/merchantmanager/Presenter/UserManagerPresenter.java
@@ -1,7 +1,19 @@
 package com.iotek.merchantmanager.Presenter;
 
+import com.google.gson.Gson;
+import com.iotek.merchantmanager.Utils.LogUtil;
 import com.iotek.merchantmanager.base.BasePresenter;
 import com.iotek.merchantmanager.base.IMvpView;
+import com.iotek.merchantmanager.bean.UserManagerVO;
+import com.iotek.merchantmanager.net.HttpExecutor;
+import com.iotek.merchantmanager.net.OnResponseListener;
+import com.iotek.merchantmanager.view.LoadingDialog;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import okhttp3.RequestBody;
+import retrofit2.Call;
 
 /**
  * Created by admin on 2017/8/29.
@@ -10,7 +22,40 @@ import com.iotek.merchantmanager.base.IMvpView;
 public class UserManagerPresenter extends BasePresenter<UserManagerPresenter.MvpView> {
 
 
-    public interface MvpView extends IMvpView{
+    private final int LIMIT_SIZE = 10;
+
+    private int currentPage = 1;
+
+
+    public void queryUser(long custId, long rootId, String uuId, int page, boolean showDialog) {
+
+        LoadingDialog dialog = new LoadingDialog(getContext());
+        if (showDialog) {
+            dialog.show();
+        }
+
+        Gson gson = new Gson();
+        Map<String, String> paramsMap = new HashMap<>();
+        paramsMap.put("custId", custId + "");
+        paramsMap.put("rootId", rootId + "");
+        paramsMap.put("uuid", uuId);
+        paramsMap.put("limit", LIMIT_SIZE + "");
+        paramsMap.put("page", page + "");
+        String paramsJson = gson.toJson(paramsMap);
+
+        RequestBody body = RequestBody.create(HttpExecutor.MEDIA_TYPE, paramsJson);
+        Call<UserManagerVO> call = mApiService.queryUser(body);
+        call.enqueue(new OnResponseListener<UserManagerVO>(getContext(),true) {
+            @Override
+            public void onSuccess(UserManagerVO userManagerVO) {
+                LogUtil.e("=====>>>>" + userManagerVO.toString());
+            }
+        });
+
+    }
+
+
+    public interface MvpView extends IMvpView {
 
     }
 }
