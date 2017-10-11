e88c4f087e5064932f1e0db3d2878e255a88e7d0
SSSSSPawn <swmyun@163.com>
2017-10-11 18:04:38 +0800

    --报表ui 搭建--

--- app/src/main/java/com/iotek/merchantmanager/fragment/BECTabFragment.java
+++ app/src/main/java/com/iotek/merchantmanager/fragment/BECTabFragment.java
@@ -1,14 +1,16 @@
-package com.iotek.merchantmanager.activity;
+package com.iotek.merchantmanager.fragment;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 
 import com.iotek.merchantmanager.Utils.AppUtils;
-import com.iotek.merchantmanager.base.BaseActivity;
+import com.iotek.merchantmanager.base.BECFragment;
 import com.iotek.merchantmanager.bean.TabMenus;
 
 import butterknife.Bind;
@@ -18,30 +20,36 @@ import iotek.com.merchantmanager.R;
  * Created by admin on 2017/10/10.
  */
 
-public abstract class BECTabActivity extends BaseActivity implements TabLayout.OnTabSelectedListener{
+public abstract class BECTabFragment extends BECFragment implements TabLayout.OnTabSelectedListener {
 
     @Bind(R.id.tab_menu) TabLayout mTabMenu;
 
     protected int mCurrentPosition = 0;
 
+    @Nullable
     @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_bec_tab, container, false);
+    }
 
-        mTabMenu.setTabMode(TabLayout.MODE_SCROLLABLE);
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        mTabMenu.setTabMode(TabLayout.MODE_FIXED);
         mTabMenu.setOnTabSelectedListener(this);
         mTabMenu.setSelectedTabIndicatorColor(getIndicatorColor());
 
-        for (int i = 0 ;i<getTabMenu().length;i++){
+        for (int i = 0; i < getTabMenu().length; i++) {
             mTabMenu.addTab(mTabMenu.newTab().setCustomView(getTabView(i)));
         }
 
         setSelectedTab(mCurrentPosition);
-        getSupportFragmentManager().beginTransaction().add(R.id.content, getTabMenu()[mCurrentPosition].mFragment).commit();
+        getChildFragmentManager().beginTransaction().add(R.id.fl_content, getTabMenu()[mCurrentPosition].mFragment).commit();
 
     }
 
-    protected void setSelectedTab(int position){
+    protected void setSelectedTab(int position) {
         mTabMenu.getTabAt(position).select();
         switchFragment(position);
     }
@@ -50,34 +58,29 @@ public abstract class BECTabActivity extends BaseActivity implements TabLayout.O
         if (position == mCurrentPosition) {
             return;
         }
-        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        FragmentTransaction transaction = getChildFragmentManager().beginTransaction();
         Fragment to = getTabMenu()[position].mFragment;
         Fragment from = getTabMenu()[mCurrentPosition].mFragment;
         mCurrentPosition = position;
 
         if (!to.isAdded()) {    // 先判断是否被add过
-            transaction.hide(from).add(R.id.content, to).commit(); // 隐藏当前的fragment，add下一个到Activity中
+            transaction.hide(from).add(R.id.fl_content, to).commit(); // 隐藏当前的fragment，add下一个到Activity中
         } else {
             transaction.hide(from).show(to).commit(); // 隐藏当前的fragment，显示下一个
         }
     }
 
-    protected int getIndicatorColor(){
+    protected int getIndicatorColor() {
         return getResources().getColor(R.color.becPrimaryBlue);
     }
 
-    protected View getTabView(int position){
-        return AppUtils.getCustomTextTabView(this,getTabMenu()[position].name);
+    protected View getTabView(int position) {
+        return AppUtils.getCustomTextTabView(getActivity(), getTabMenu()[position].name);
     }
 
     @Override
     protected boolean isBindEventBus() {
         return false;
-    }
-
-    @Override
-    protected int getLayoutID() {
-        return R.layout.activity_bec_tab;
     }
 
     @Override
---
+++ app/src/main/java/com/iotek/merchantmanager/fragment/CrkGoodsListFragment.java
@@ -0,0 +1,40 @@
+package com.iotek.merchantmanager.fragment;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.widget.RecyclerView;
+
+import com.iotek.merchantmanager.adapter.UserManagerAdapter;
+import com.iotek.merchantmanager.base.ListFragment;
+
+import iotek.com.merchantmanager.R;
+
+/**
+ * Created by admin on 2017/10/11.
+ */
+
+public class CrkGoodsListFragment extends ListFragment {
+
+    UserManagerAdapter mAdapter;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mAdapter = new UserManagerAdapter();
+    }
+
+    @Override
+    protected RecyclerView.Adapter getAdapter() {
+        return mAdapter;
+    }
+
+    @Override
+    protected int getLayoutId() {
+        return R.layout.fragment_base_list;
+    }
+
+    @Override
+    protected boolean isBindEventBus() {
+        return false;
+    }
+}
