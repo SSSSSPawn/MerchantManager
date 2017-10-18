d8a8303110691c9deff3a65311e6f3e4065e7f3c
SSSSSPawn <swmyun@163.com>
2017-10-18 11:10:14 +0800

    --报表UI 修改--

--- app/src/main/java/com/iotek/merchantmanager/Presenter/TradeFormDetailPresenter.java
+++ app/src/main/java/com/iotek/merchantmanager/Presenter/TradeFormDetailPresenter.java
@@ -2,6 +2,11 @@ package com.iotek.merchantmanager.Presenter;
 
 import com.iotek.merchantmanager.base.BasePresenter;
 import com.iotek.merchantmanager.base.IMvpView;
+import com.iotek.merchantmanager.bean.TradeFormDetailVO;
+import com.iotek.merchantmanager.bean.params.TradeFormDetailParamsVO;
+import com.iotek.merchantmanager.net.OnResponseListener;
+
+import retrofit2.Call;
 
 /**
  * Created by admin on 2017/10/17.
@@ -9,8 +14,14 @@ import com.iotek.merchantmanager.base.IMvpView;
 
 public class TradeFormDetailPresenter extends BasePresenter<TradeFormDetailPresenter.MvpView> {
 
-    public void getTradeFormDetailList(){
+    public void getTradeFormDetailList(TradeFormDetailParamsVO paramsVO){
+        Call<TradeFormDetailVO> call = mApiService.getDayTradeDetail(paramsVO);
+        call.enqueue(new OnResponseListener<TradeFormDetailVO>(getContext(),false) {
+            @Override
+            public void onSuccess(TradeFormDetailVO tradeFormDetailVO) {
 
+            }
+        });
     }
 
 
